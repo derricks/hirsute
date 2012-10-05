1cece119917da701cf6b8a38ee37c314cbcf9e54
Derrick Schneider <derrick.schneider@gmail.com>
2012-10-04 22:18:33 -0700

    Make initial unit test pass (proper distribution in a very skewed probability curve)

--- tests/hirsute_test.rb
+++ tests/hirsute_test.rb
@@ -10,16 +10,15 @@ class TestHiruste < Test::Unit::TestCase
     
     # define a very skewed histogram
     histogram_a = [0.9,0.05,0.05]
-    values_a = Array.new(0)
+    values_a = Array.new(histogram_a.length,0)
     
     (1..1000).each do |i|
       index = integer_from_histogram(histogram_a)
-      puts index
       values_a[index] = values_a[index] + 1
     end
     
-    # check for 10% tolerance
-    assert(values_a[0] > 810 && values_a[0] < 990)
+    # check for 5% tolerance
+    assert(values_a[0] > 855 && values_a[0] < 945)
     
   end
 end
